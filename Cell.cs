2bbfa98054fff0908e081ac3d74808d91dd75cd0
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-10-02 22:35:25 +0800

    Create Cell.cs

---
+++ Cell.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+using Raylib_cs;
+namespace NotQuiteTetris;
+
+public class Cell {
+	public Vector2 Position { get; private set; }
+	
+	public Color Color { get; private set; }
+
+	public Cell(Vector2 position, Color color) {
+		Log.Me(() => "Creating new cell...");
+		Position = position;
+		Color = color;
+
+		Log.Me(() => "Done!");
+	}
+}
