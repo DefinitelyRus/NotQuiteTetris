690f7395c6d7a411364f6807c441f0361151c2ef
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-10-07 18:41:50 +0800

    Update GridRenderer.cs

--- GridRenderer.cs
+++ GridRenderer.cs
@@ -7,7 +7,7 @@ public static class GridRenderer {
 
 	public static int PosY => 100;
 
-	public static int Padding => 5;
+	public static int CellOutlineWidth => 4;
 
 	public static int Spacing => 2;
 
@@ -25,6 +25,7 @@ public static class GridRenderer {
 		}
 	}
 
+
 	public static void Update(bool v = false, int s = 0) {
 		Log.Me(() => "Rendering grid...", v, s + 1);
 
@@ -34,26 +35,27 @@ public static class GridRenderer {
 
 				int cellPosX = x * CellSize + PosX;
 				int cellPosY = y * CellSize + PosY;
+				Rectangle rect = new(cellPosX, cellPosY, CellSize, CellSize);
 
 				// Has cell
 				if (cell != null) {
 					Log.Me(() => $"Drawing cell at ({x}, {y})...", v, s + 1);
 					Raylib.DrawRectangle(cellPosX, cellPosY, CellSize, CellSize, cell.Color);
+					Raylib.DrawRectangleLinesEx(rect, CellOutlineWidth, Color.Black);
 				}
 				
 				// No cell
 				else {
 					Log.Me(() => $"Drawing empty cell at ({x}, {y})...", v, s + 1);
-					Rectangle rect = new(cellPosX, cellPosY, CellSize, CellSize);
 					Raylib.DrawRectangle(cellPosX, cellPosY, CellSize, CellSize, Color.DarkGray);
-					Raylib.DrawRectangleLinesEx(rect, Padding, Color.Gray);
+					Raylib.DrawRectangleLinesEx(rect, CellOutlineWidth, Color.Gray);
 				}
 			}
 		}
 
-
 		Log.Me(() => "Done!", v, s + 1);
 	}
+
 
 	public static void PreviewBlock(Block block, int gridX, int gridY, bool v = false, int s = 0) {
 		Log.Me(() => $"Previewing block at ({gridX}, {gridY})...", v, s + 1);
@@ -63,15 +65,24 @@ public static class GridRenderer {
 
 			// For each cell (row) in the column...
 			for (int blockY = 0; blockY < block.Height; blockY++) {
+				int targetX = gridX + blockX;
+				int targetY = gridY + blockY;
+
+				// Skip if out of bounds
+				bool belowZero = targetX < 0 || targetY < 0;
+				bool beyondBounds = targetX >= GridManager.Width || targetY >= GridManager.Height;
+				if (belowZero || beyondBounds) continue;
+
 				bool blockHasCell = block.HasCellAt(blockX, blockY);
 				bool gridHasCell = GridManager.GetCell(gridX + blockX, gridY + blockY, v, s + 1) != null;
 				int cellPosX = (gridX + blockX) * CellSize + PosX;
 				int cellPosY = (gridY + blockY) * CellSize + PosY;
 
 				if (blockHasCell) {
+					Log.Me(() => $"Rendering cell at ({cellPosX}, {cellPosY})...", v, s + 1);
 					Color previewColor = gridHasCell ? Color.Red : Color.Green;
 					Raylib.DrawRectangle(cellPosX, cellPosY, CellSize, CellSize, previewColor);
-					Raylib.DrawRectangleLinesEx(new Rectangle(cellPosX, cellPosY, CellSize, CellSize), Padding, Color.Black);
+					Raylib.DrawRectangleLinesEx(new Rectangle(cellPosX, cellPosY, CellSize, CellSize), CellOutlineWidth, Color.Black);
 				}
 			}
 		}
