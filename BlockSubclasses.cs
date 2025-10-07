09e77aebe802bc5fcc9e9a5cb8f77e62b20ad92f
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-10-07 18:41:36 +0800

    Update BlockSubclasses.cs

--- BlockSubclasses.cs
+++ BlockSubclasses.cs
@@ -1,5 +1,4 @@
-﻿using Raylib_cs;
-namespace NotQuiteTetris;
+﻿namespace NotQuiteTetris;
 
 public class IBlock : Block {
 	public IBlock(
@@ -9,10 +8,116 @@ public class IBlock : Block {
 		grid: [
 			[true, true, true, true]
 		],
-		color: Color.SkyBlue,
+		color: GameRenderer.ColorPalette["IBlock"],
 		v: v,
 		s: s + 1
 	) {
 		Log.Me(() => "Created IBlock.", v, s + 1);
+	}
+}
+
+
+public class JBlock : Block {
+	public JBlock(
+		bool v = false,
+		int s = 0
+	) : base(
+		grid: [
+			[true],
+			[true, true, true]
+		],
+		color: GameRenderer.ColorPalette["JBlock"],
+		v: v,
+		s: s + 1
+	) {
+		Log.Me(() => "Created JBlock.", v, s + 1);
+	}
+}
+
+public class LBlock : Block {
+	public LBlock(
+		bool v = false,
+		int s = 0
+	) : base(
+		grid: [
+			[false, false, true],
+			[true, true, true]
+		],
+		color: GameRenderer.ColorPalette["LBlock"],
+		v: v,
+		s: s + 1
+	) {
+		Log.Me(() => "Created LBlock.", v, s + 1);
+	}
+}
+
+
+public class OBlock : Block {
+	public OBlock(
+		bool v = false,
+		int s = 0
+	) : base(
+		grid: [
+			[true, true],
+			[true, true]
+		],
+		color: GameRenderer.ColorPalette["OBlock"],
+		v: v,
+		s: s + 1
+	) {
+		Log.Me(() => "Created OBlock.", v, s + 1);
+	}
+}
+
+
+public class SBlock : Block {
+	public SBlock(
+		bool v = false,
+		int s = 0
+	) : base(
+		grid: [
+			[false, true, true],
+			[true, true, false]
+		],
+		color: GameRenderer.ColorPalette["SBlock"],
+		v: v,
+		s: s + 1
+	) {
+		Log.Me(() => "Created SBlock.", v, s + 1);
+	}
+}
+
+
+public class ZBlock : Block {
+	public ZBlock(
+		bool v = false,
+		int s = 0
+	) : base(
+		grid: [
+			[true, true, false],
+			[false, true, true]
+		],
+		color: GameRenderer.ColorPalette["ZBlock"],
+		v: v,
+		s: s + 1
+	) {
+		Log.Me(() => "Created ZBlock.", v, s + 1);
+	}
+}
+
+
+public class DotBlock : Block {
+	public DotBlock(
+		bool v = false,
+		int s = 0
+	) : base(
+		grid: [
+			[true]
+		],
+		color: GameRenderer.ColorPalette[".Block"],
+		v: v,
+		s: s + 1
+	) {
+		Log.Me(() => "Created DotBlock.", v, s + 1);
 	}
 }
