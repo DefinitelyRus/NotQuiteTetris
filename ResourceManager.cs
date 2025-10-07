8517364fc6af46eca154dc70a74f30f6187e2a2b
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-10-07 18:41:56 +0800

    Update ResourceManager.cs

--- ResourceManager.cs
+++ ResourceManager.cs
@@ -7,6 +7,21 @@ internal static class ResourceManager {
 
 	public static Music MusicStream = new();
 
+	public const string Title = "NotQuiteTetris";
+
+	public static string SaveFilePath { get; private set; } = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/" + Title + "save.json";
+
+
+	public static void Initialize(bool v = false, int s = 0) {
+		Log.Me(() => "Initializing resource manager...", v, s + 1);
+
+		Log.Me(() => "Initializing audio device...", v, s + 1);
+		Raylib.InitAudioDevice();
+		Raylib.SetMasterVolume(0.6f);
+
+		Log.Me(() => "Done!", v, s + 1);
+	}
+
 
 	public static void PreloadAssets(bool v = false, int s = 0) {
 		Log.Me(() => "Preloading assets...", v, s + 1);
@@ -17,7 +32,16 @@ internal static class ResourceManager {
 		foreach (string file in textureFiles) {
 			string name = Path.GetFileNameWithoutExtension(file);
 			Log.Me(() => $"Loading texture \"{name}\" from \"{file}\"...", v, s + 1);
-			Texture2D texture = Raylib.LoadTexture(file);
+			Texture2D texture = new();
+
+			try {
+				texture = Raylib.LoadTexture(file);
+			}
+			catch (AccessViolationException) {
+				Log.Err($"Unable to access file \"{name}\" from \"{file}\".", true, s + 1);
+				continue;
+			}
+
 			Textures[name] = texture;
 		}
 
@@ -66,13 +90,43 @@ internal static class ResourceManager {
 		Raylib.UpdateMusicStream(MusicStream);
 		if (Raylib.IsMusicStreamPlaying(MusicStream)) return;
 
-		Log.Me(() => "Finding background music to play...", v, s + 1);
-		int musicIndex = Raylib.GetRandomValue(0, 4);
-		string filePath = $"Assets\\Audio\\music{musicIndex}.wav";
+		Log.Me(() => "Finding tetoris...", v, s + 1);
+		string filePath = $"Assets\\Audio\\tetoris.wav";
 		string fileName = Path.GetFileNameWithoutExtension(filePath);
 		MusicStream = Raylib.LoadMusicStream(filePath);
 
 		Raylib.PlayMusicStream(MusicStream);
 		Log.Me(() => $"Now playing: {fileName}.", v, s + 1);
+	}
+
+
+	public static void SaveToFile(string content, bool v = false, int s = 0) {
+		Log.Me(() => $"Saving to file \"{SaveFilePath}\"...", v, s + 1);
+
+		try {
+			File.WriteAllText(SaveFilePath, content);
+		}
+
+		catch (Exception e) {
+			Log.Err(() => $"Failed to save to file \"{SaveFilePath}\": {e.Message}", true, s + 1);
+			return;
+		}
+	}
+
+
+	public static string? LoadFromFile(bool v = false, int s = 0) {
+		Log.Me(() => $"Loading from file \"{SaveFilePath}\"...", v, s + 1);
+		string content;
+
+		try {
+			content = File.ReadAllText(SaveFilePath);
+		}
+
+		catch (Exception e) {
+			Log.Err(() => $"Failed to load from file \"{SaveFilePath}\": {e.Message}", true, s + 1);
+			return null;
+		}
+
+		return content;
 	}
 }
